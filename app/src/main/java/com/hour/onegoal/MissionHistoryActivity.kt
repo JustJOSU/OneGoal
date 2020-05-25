b7f9b556db7e34351c751afeb23c1f2c363107ae
Rabbitg <rmsgml7707@gmail.com>
2020-05-25 21:41:21 +0900

    layout

---
+++ app/src/main/java/com/hour/onegoal/MissionHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.hour.onegoal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MissionHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mission_history)
+    }
+}
