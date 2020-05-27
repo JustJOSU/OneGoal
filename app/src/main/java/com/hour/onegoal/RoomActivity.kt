77d199f9b4bd5f644d6af165bb0732750580cef6
SungJaeJO <xc11161@gmail.com>
2020-05-27 21:35:15 +0900

    mission submit dialog update

--- app/src/main/java/com/hour/onegoal/RoomActivity.kt
+++ app/src/main/java/com/hour/onegoal/RoomActivity.kt
@@ -1,9 +1,14 @@
 package com.hour.onegoal
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_room.*
 import soup.neumorphism.NeumorphTextView
 
@@ -24,5 +29,30 @@ class RoomActivity : AppCompatActivity() {
             intent.putExtra("roomId",roomId)
             startActivity(intent)
         }
+
+        submit_cardView.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.custom_dialog)
+        val body = dialog.findViewById(R.id.dialog_missionTitle) as TextView
+        body.text = title
+        val photo = dialog.findViewById(R.id.dialog_imageView) as ImageView
+        Glide.with(this)
+            .load(photo)
+            .into(photo)
+        val yesBtn = dialog.findViewById(R.id.okButton) as Button
+        val noBtn = dialog.findViewById(R.id.cancelButton) as Button
+        yesBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+        noBtn.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+
     }
 }
